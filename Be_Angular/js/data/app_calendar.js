222541477ad6cb860ce0cfd35806aedcbaf5f0b1
liumengwei <liumengwei>
2018-11-17 16:11:08 +0800

    add flower_info page,and save flower_info page

--- Be_Angular/js/data/app_calendar.js
+++ Be_Angular/js/data/app_calendar.js
@@ -7,15 +7,28 @@ $(document).ready(function () {
         success: function (data) {
             var info = data.body;
             for (var i = 0; i < info.length; i++) {
-                $('.flowerInfo').append('<tr >\n' +
+                var deleteStatus = info[i].delete;
+                var deleteStr = '是';
+                if (deleteStatus === 0) {
+                    deleteStr = '否';
+                }
+                $('.flowerInfo').append(
+                    '<tr >\n' +
                     '                                    <td>' + info[i].id + '</td>\n' +
                     '                                    <td>' + info[i].flowerImg + '</td>\n' +
-                    '                                    <td><a href>' + info[i].flowerName + '</a></td>\n' +
+                    '                                    <td>' + info[i].flowerName + '</td>\n' +
                     '                                    <td>' + info[i].flowerLanguage + '</td>\n' +
                     '                                    <td data-value="78025368997">' + info[i].createTime + '</td>\n' +
-                    '                                    <td data-value="1"><span class="label bg-success" title="Active">' + info[i].delete + '</span></td>\n' +
+                    '                                    <td data-value="1"><span class="label bg-success" title="Active">' + deleteStr + '</span></td>\n' +
+                    '                                    <td><button type="button" class="btn btn-sm btn-primary " ng-click="myModel.show()"><i></i>查看</button></td>' +
                     '                                </tr>');
             }
         }
     })
 });
+
+
+
+function addInfo() {
+
+}
