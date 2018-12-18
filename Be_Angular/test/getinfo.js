8c3fe44f723425680e58c2c880e6e31cfc947342
liumengwei <lmwffdbb@gmail.com>
2018-12-18 13:54:26 +0800

    git delete file and add zip

--- Be_Angular/test/getinfo.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("app", []);
-app.controller('getInfoCon', function ($scope, $http) {
-    $http.get('http://localhost:17900/flower/flowers?flowerName').then(function (result) {
-        $scope.infos = result.data.body;
-    })
-})
